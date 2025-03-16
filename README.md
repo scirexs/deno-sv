9c65f053ad2074d97e9b2801a6defc2ac28aa18d
scirexs <git@scirexs.dev>
2025-03-16 15:10:51 +0000

    Testing release.

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # deno-sv
-Setup Svelte on Deno until officially supported.
+
+[![JSR](https://jsr.io/badges/@scirexs/sv)](https://jsr.io/@scirexs/sv)
+
+Setup svelte project by deno project structure in a simple way.
+
+## Quick Start
+1. Move to project directory.
+2. Run command as below
+```sh
+deno run jsr:@scirexs/sv
+```
+
+## Options
+### Additional setup for vitest
+```sh
+deno run jsr:@scirexs/sv --vitest
+```
+
+### Additional setup for tailwind css
+```sh
+deno run jsr:@scirexs/sv --tailwind
+```
+
+### Skip interactions
+```sh
+deno run jsr:@scirexs/sv --no-confirm
+```
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,28 @@
 import * as p from "jsr:@std/path@^1.0.8";
+import { UntarStream } from "jsr:@std/tar@^0.1.6";
 import { Command } from "jsr:@cliffy/command@1.0.0-rc.7";
 import { Toggle } from "jsr:@cliffy/prompt@1.0.0-rc.7";
 import $ from "jsr:@david/dax@^0.42.0";
+import { JSRPackage } from "./package_downloader.ts";
 import packageInfo from "../deno.json" with { type: "json" };
 
 /** Entrypoint of deno-sv. */
 export default async function main() {
   const { options } = await parseCommand();
+  const root = await confirmProjectRoot(options.confirm);
+  if (!root) return console.error(`cancelled`);
+
+  const tmp = Deno.makeTempDirSync();
   try {
-    const dir = await confirmProjectRoot(options.confirm);
-    console.log(`${options.vitest} ${options.tailwind} ${dir}`);
+    const source = await prepareSetup(tmp);
+    await setupSvelte(source, root);
+    if (options.vitest) await setupVitest(source, root);
+    if (options.tailwind) await setupTailwind(source, root);
+    replaceViteConfig(root, options.vitest, options.tailwind);
   } catch (e) {
     showError(e);
+  } finally {
+    Deno.removeSync(tmp, { recursive: true });
   }
 }
 
@@ -28,12 +39,100 @@ async function parseCommand() {
 async function confirmProjectRoot(confirm?: boolean): Promise<string> {
   const cwd = p.resolve(".");
   if (!confirm) return cwd;
-  if (!(await Toggle.prompt(`Setup the dir as svelte project: ${cwd}`))) throw new Error("cancelled");
+  if (!(await Toggle.prompt(`Setup the dir as svelte project: ${cwd}`))) return "";
   return cwd;
+}
+async function prepareSetup(tmp: string): Promise<string> {
+  const pkg = p.join(tmp, "pkg.tgz");
+  await new JSRPackage("scirexs", "sv").downloadPackage(pkg);
+  await decompressTgz(pkg);
+  return p.join(tmp, "package", "templates");
+}
+async function setupSvelte(source: string, root: string) {
+  const ROOT_FILES = ["deno.json", "tsconfig.json", "vite.config.ts", "svelte.config.mjs", ".gitignore"];
+  const SRC_FILES = ["app.html"];
+  const ROUTES_FILES = ["+page.svelte", "+layout.server.ts", "+error.svelte"];
+  const STATIC_FILES = ["favicon.svg", "favicon.ico", "apple-touch-icon.png"];
+  const PACKAGES = [
+    "vite",
+    "svelte",
+    "svelte-check",
+    "@sveltejs/kit",
+    "@sveltejs/vite-plugin-svelte",
+    "@deno/vite-plugin",
+    "@sveltejs/adapter-static",
+  ];
+
+  const src = p.join(root, "src");
+  Deno.mkdirSync(p.join(src, "lib"), { recursive: true });
+  copyFilesWithMakeDir(source, root, ROOT_FILES);
+  copyFilesWithMakeDir(source, src, SRC_FILES);
+  copyFilesWithMakeDir(source, p.join(src, "routes"), ROUTES_FILES);
+  copyFilesWithMakeDir(source, p.join(root, "static"), STATIC_FILES);
+  await addNpmPackage(PACKAGES);
+}
+async function setupVitest(source: string, root: string) {
+  const TESTS_FILES = ["setup.ts", "unit.test.ts"];
+  const PACKAGES = ["vitest", "jsdom", "@testing-library/jest-dom", "@testing-library/svelte", "@testing-library/user-event"];
+
+  copyFilesWithMakeDir(source, p.join(root, "tests"), TESTS_FILES);
+  await addNpmPackage(PACKAGES);
+}
+async function setupTailwind(source: string, root: string) {
+  const SRC_FILES = ["app.css"];
+  const ROUTES_FILES = ["+layout.svelte"];
+  const PACKAGES = ["tailwindcss", "@tailwindcss/vite"];
+
+  copyFilesWithMakeDir(source, p.join(root, "src"), SRC_FILES);
+  copyFilesWithMakeDir(source, p.join(root, "src", "routes"), ROUTES_FILES);
+  await addNpmPackage(PACKAGES);
+}
+async function decompressTgz(path: string) {
+  const dir = p.dirname(path);
+  for await (
+    const entry of (await Deno.open(path)).readable
+      .pipeThrough(new DecompressionStream("gzip"))
+      .pipeThrough(new UntarStream())
+  ) {
+    const target = p.normalize(p.join(dir, entry.path));
+    Deno.mkdirSync(p.dirname(target), { recursive: true });
+    await entry.readable?.pipeTo((await Deno.create(target)).writable);
+  }
+}
+async function addNpmPackage(packages: string[]) {
+  for (const name of packages) {
+    await $`deno add npm:${name}`;
+  }
+}
+async function replaceViteConfig(root: string, vitest?: boolean, tailwind?: boolean) {
+  const path = p.join(root, "vite.config.ts");
+  await replaceViteConfigForVitest(path, vitest);
+  await replaceViteConfigForTailwind(path, tailwind);
+}
+async function replaceViteConfigForVitest(path: string, vitest?: boolean) {
+  if (vitest) {
+    await $`sed -i 's:// ::' ${path}`;
+  } else {
+    await $`sed -i '/^\s*\/\/ / d' ${path}`;
+  }
+}
+async function replaceViteConfigForTailwind(path: string, tailwind?: boolean) {
+  if (tailwind) {
+    await $`sed -i 's:///::' ${path}`;
+  } else {
+    await $`sed -i '/^\s*\/\/\// d' ${path}`;
+  }
+}
+function copyFilesWithMakeDir(from: string, to: string, files: string[]) {
+  Deno.mkdirSync(to, { recursive: true });
+  files.forEach((file) => copy(from, to, file));
+}
+function copy(from: string, to: string, file: string) {
+  Deno.copyFileSync(p.join(from, file), p.join(to, file));
 }
 function showError(e: unknown) {
   if (e instanceof Error) {
-    console.log(`error: ${e.message}`);
+    console.error(`error: ${e.message}`);
   } else {
     throw e;
   }
